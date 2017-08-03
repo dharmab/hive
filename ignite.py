ea09305e6bc01583ca889c8e5551faf59ce5cbcf
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2017-08-03 17:25:34 -0600

    Put service data on filesystem

--- ignite.py
+++ ignite.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import json
 import sys
 import textwrap
 import yaml
@@ -43,14 +44,68 @@ def dropin(name, *, contents):
     }
 
 
+def _file(path, *, contents='', mode=644):
+    return {
+        'filesystem': 'root',
+        'path': path,
+        'contents': {
+            'inline': contents
+        },
+        'mode': mode
+    }
+
+
+def swarm_service(
+    name,
+    *,
+    image,
+    tag='latest',
+    environment={},
+    ports=[],
+    bind_mounts={},
+    is_global=False,
+    replicas=1
+):
+    return {
+        'name': name,
+        'image': '{}:{}'.format(image, tag),
+        'environment': environment,
+        'ports': ports,
+        'bind_mounts': bind_mounts,
+        'global': is_global,
+        'replicas': replicas
+    }
+
+
+def port(*, host_port, container_port, protocol='tcp'):
+    return {
+        'host': host_port,
+        'container': container_port,
+        'protocol': protocol
+    }
+
+
+def bind_mount(*, host_path, container_path, access='rw'):
+    return {
+        'host': host_path,
+        'container': container_path,
+        'access': access
+    }
+
+
 docker_swarm_dropin = dropin(
     'docker-swarm.conf',
     contents="""
     [Service]
     ExecStartPost=/usr/bin/docker swarm init
     """
-
 )
+
+default_services = [
+    swarm_service('nginx', image='nginx', ports=[
+        port(host=8080, container=80)
+    ]),
+]
 
 ignition = {
     'systemd': {
@@ -58,6 +113,11 @@ ignition = {
             unit('docker.service', enabled=True, dropins=[docker_swarm_dropin]),
         ]
     },
+    'storage': {
+        'files': [
+            _file('/etc/swarm-services.json', mode=600, contents=json.dumps(default_services))
+        ]
+    }
 }
 
 
